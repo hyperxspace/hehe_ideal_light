9b95768197aac4e5c1cd875daa9dc4db05fc1f29
thewinds <wind_ns@163.com>
2018-04-17 17:18:54 +0800

    添加命令行循环

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var question_fetcher = require("./question_fetcher/")
+var readlineSync = require('readline-sync');
 
-question_fetcher.get_question_text()
-    .then(question => {
-        console.log("== 问题 ==")
-        console.log(question)
-    })
+const Koa = require('koa')
+const KoaStatic = require('koa-static')
+const app = new Koa()
+
+app.use(KoaStatic("./static"))
+app.listen(8080)
+
+async function main(){
+    while (true) {
+        await question_fetcher
+        .get_question_text()
+        .then(question => {
+            console.log("== 问题 ==")
+            console.log(question)
+        })
+
+        readlineSync.question("press enter to contine ...")
+    }
+}
+
+main()
