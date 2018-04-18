498ed10be90c2d980f8f381388eff4854ffb0901
inRush <in_rush@outlook.com>
2018-04-18 13:53:35 +0800

    合并修改

--- question_fetcher/ocr.js
+++ question_fetcher/ocr.js
@@ -5,16 +5,9 @@ let tesseract = Tesseract.create({
     corePath: 'https://cdn.rawgit.com/naptha/tesseract.js-core/master/index.js'
 });
 
-<<<<<<< HEAD
 const QUESTION_START_SIGN = '】';
 const QUESTION_END_SIGN = 'A';
 const QUESTION_FILENAME = 'question.png';
-=======
-const QUESTION_START_SIGN = '】'
-const QUESTION_END_SIGN = 'A'
-const QUESTION_FILENAME = 'question.png'
-const QUESTION_TITLE_MAX_LEN = 12
->>>>>>> a04f9ee5ca3dea2c0e4c26ac22977bda752bb9c3
 /**
  * 获取问题文字,返回一个Promise
  */
@@ -26,7 +19,6 @@ function get_question() {
             })
             // .progress(function  (p) { console.log('progress', p)    })
             .then(result => {
-<<<<<<< HEAD
                 let statr_index = result.text.indexOf(QUESTION_START_SIGN);
                 let is_one_choice = result.text.indexOf('单选题') != -1;
                 let question_type = is_one_choice ? 0 : 1;
@@ -43,27 +35,6 @@ function get_question() {
                     text: result.text.substring(statr_index + 1),
                     type: question_type
                 });
-=======
-                let statr_index = result.text.indexOf(QUESTION_START_SIGN)
-                let end_index = result.text.indexOf(QUESTION_END_SIGN)
-                let is_one_choice = (result.text.indexOf("单选题") != -1)
-                let question_type = is_one_choice ? 0 : 1
-                if (statr_index == -1 || end_index == -1) {
-                    resolve({
-                        text: result.text.length > QUESTION_TITLE_MAX_LEN ? 
-                            result.text.slice(5, QUESTION_TITLE_MAX_LEN) : result.text,
-                        type: question_type
-                    })
-                    return
-                }
-                resolve({
-                    text: result.text.substring(statr_index + 1, end_index),
-                    type: question_type
-                })
-
-            }).catch(err => {
-                reject(err)
->>>>>>> a04f9ee5ca3dea2c0e4c26ac22977bda752bb9c3
             })
             .catch(err => {
                 reject(err);
